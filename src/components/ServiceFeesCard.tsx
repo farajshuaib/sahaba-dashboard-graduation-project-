3e74ca179dd3c9d0da462073d80843f958a67813
faraj Shuaib <46637985+farajshuaib@users.noreply.github.com>
2022-10-24 22:05:59 +0200

    dashboard screen

---
+++ src/components/ServiceFeesCard.tsx
@@ -0,0 +1,150 @@
+import { useWeb3React } from "@web3-react/core";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "constant";
+import { Contract, utils } from "ethers";
+import { parseEther } from "ethers/lib/utils";
+import React, { useEffect } from "react";
+import { toast } from "react-toastify";
+import ButtonPrimary from "shared/Button/ButtonPrimary";
+import ButtonSecondary from "shared/Button/ButtonSecondary";
+import Input from "shared/Input/Input";
+
+const ServiceFeesCard: React.FC = () => {
+  const { account, library, active } = useWeb3React();
+  const [serviceFees, setServiceFees] = React.useState<number>(0);
+  const [newServiceFees, setNewServiceFees] = React.useState<number>(0);
+  const [updateServiceFeesLoading, setUpdateServiceFeesLoading] =
+    React.useState<boolean>(false);
+
+  const [toggleUpdateServiceFee, setToggleUpdateServiceFee] =
+    React.useState<boolean>(false);
+
+  const fetchPlatformFees = async () => {
+    if (!account || !library) {
+      return;
+    }
+    try {
+      const contract = new Contract(
+        CONTRACT_ADDRESS,
+        CONTRACT_ABI,
+        library?.getSigner()
+      );
+
+      const res = await contract.getServiceFeesPrice();
+      setServiceFees(+utils.formatEther(res).toString());
+      setNewServiceFees(+utils.formatEther(res).toString() * 100);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updatePlatformFees = async () => {
+    if (!account || !library) {
+      return;
+    }
+
+    if (newServiceFees < 0 || newServiceFees > 100) {
+      toast.error("Please enter a valid service fee percentage");
+      return;
+    }
+
+    setUpdateServiceFeesLoading(true);
+
+    try {
+      const contract = new Contract(
+        CONTRACT_ADDRESS,
+        CONTRACT_ABI,
+        library?.getSigner()
+      );
+
+      const isApprovedForAll = await contract.isApprovedForAll(
+        account,
+        CONTRACT_ADDRESS
+      );
+
+      if (!isApprovedForAll) {
+        await contract.setApprovalForAll(CONTRACT_ADDRESS, true);
+      }
+
+      const tx = await contract.setServiceFeesPrice(
+        parseEther((newServiceFees / 100).toString())
+      );
+      await tx.wait();
+      await fetchPlatformFees();
+      setUpdateServiceFeesLoading(false);
+      toast.success("Service fees updated successfully");
+    } catch (error) {
+      toast.error("Something went wrong");
+      setUpdateServiceFeesLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlatformFees();
+  }, [account, active]);
+
+  return (
+    <section className="relative overflow-hidden bg-white shadow-lg sm:rounded-lg col-span-12 lg:col-span-8">
+      <div className="px-6 py-4 bg-white border-b border-gray-200">
+        <h6 className="text-xl font-semibold">Platform Service fees</h6>
+      </div>
+      {!account ? (
+        <p className="px-6 py-4 text-sm text-red-500">
+          * You need to be connected to your wallet to interact with the
+          blockchain.
+        </p>
+      ) : (
+        <div className="px-6 py-4">
+          <p className="">
+            Platform owner will receive fees on each purchase activity
+          </p>
+
+          <div className="flex items-center my-5 gap-3">
+            {toggleUpdateServiceFee ? (
+              <>
+                <div className="relative flex w-full">
+                  <span className="inline-flex font-bold items-center px-3 text-sm border border-r-0 rounded-l-2xl border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 text-neutral-500 dark:text-neutral-400">
+                    %
+                  </span>
+                  <Input
+                    type="number"
+                    value={newServiceFees}
+                    min={0}
+                    name="serviceFees"
+                    max={100}
+                    onChange={(e) => {
+                      setNewServiceFees(+e.target.value);
+                    }}
+                    className="!rounded-l-none w-full"
+                  />
+                </div>
+                <ButtonPrimary
+                  loading={updateServiceFeesLoading}
+                  onClick={updatePlatformFees}
+                >
+                  conform
+                </ButtonPrimary>
+              </>
+            ) : (
+              <h3 className="text-gray-800 text-xl font-bold">
+                {serviceFees * 100}%{" "}
+                <span className="text-primary-700 text-base">ETH</span>
+              </h3>
+            )}
+          </div>
+
+          <div className="flex justify-end">
+            <ButtonSecondary
+              onClick={() => setToggleUpdateServiceFee(!toggleUpdateServiceFee)}
+              className=""
+            >
+              {toggleUpdateServiceFee ? "Cancel update" : "update service fees"}
+            </ButtonSecondary>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default ServiceFeesCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,330 @@
-import React from 'react'
+import LoadingScreen from "components/LoadingScreen";
+import { useCrud } from "hooks/useCrud";
+import React from "react";
+import { useEffect } from "react";
+import logo_light from "../assets/logo_light.svg";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
 
-const Home:React.FC = () => {
+import { useApi } from "hooks/useApi";
+import ServiceFeesCard from "components/ServiceFeesCard";
+import { Line } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
+import ServerError from "components/ServerError";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  ArcElement,
+  Legend
+);
+
+export const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const Home: React.FC = () => {
+  const api = useApi();
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [errors, setErrors] = React.useState<boolean>(false);
+  const [kycData, setKycData] = React.useState<any>();
+  const [transactionData, setTransactionData] = React.useState<any>();
+  const [categoriesNftsData, setCategoriesNftsData] = React.useState<any>();
+
+  const fetchData = async () => {
+    try {
+      const { data: transaction } = await api.get("/statistics/transactions");
+      const { data: kyc } = await api.get("/statistics/kyc");
+      const { data: categoriesNfts } = await api.get(
+        "/statistics/categories-nfts"
+      );
+      setKycData(kyc.data);
+      setTransactionData(transaction.data);
+      setCategoriesNftsData(categoriesNfts.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setErrors(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
+  if (errors) {
+    return <ServerError />;
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-12">
+      <section className="relative overflow-hidden  shadow-lg sm:rounded-lg col-span-12 text-white lg:col-span-4 bg-primary-700">
+        <div className="px-6 py-4">
+          <div className="flex flex-wrap items-center gap-4 py-4">
+            <img src={logo_light} alt="" className="w-24 h-24 object-cover" />
+            <div className="flex flex-col gap-2">
+              <span className="font-semibold uppercase">Total Sold Tokens</span>
+              <div className="flex items-end gap-2">
+                <span className="text-2xl">
+                  {transactionData.count?.total_sold_tokens}
+                  <span className="text-xs text-gray-100">NFTs</span>
+                </span>
+              </div>
+            </div>
+          </div>
+          <p className="mt-4 mb-2 font-semibold uppercase">
+            Sold amount in each token
+          </p>
+          <div className="flex flex-wrap gap-4">
+            <div>
+              <p>
+                {transactionData.count?.total_sold_amount}
+                <span className="text-xs text-gray-100"> ETH</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
-export default Home
+      <ServiceFeesCard />
+
+      <section className="relative overflow-hidden bg-white shadow-lg sm:rounded-lg col-span-12 lg:col-span-8">
+        <div className="px-6 py-4 bg-white border-b border-gray-200">
+          <div className="text-xl font-semibold">
+            <div className="flex items-center justify-between">
+              <p className="text-xl font-semibold">Transactions</p>
+              <p className="text-sm text-gray-500">Last 30 days</p>
+            </div>
+          </div>
+        </div>
+        <div className="px-6 py-4">
+          {/* chart */}
+          <Line
+            options={{
+              responsive: true,
+              plugins: {
+                legend: {
+                  position: "top" as const,
+                },
+                title: {
+                  display: true,
+                  text: "Transactions events",
+                },
+              },
+            }}
+            data={{
+              labels: transactionData.statistics?.labels,
+              datasets: [
+                {
+                  label: "mint",
+                  data: transactionData.statistics?.mint,
+                  borderColor: "rgb(4, 108, 78)",
+                  backgroundColor: "rgba(4, 108, 78, 0.5)",
+                },
+                {
+                  label: "sale",
+                  data: transactionData.statistics?.sale,
+                  borderColor: "rgb(53, 162, 235)",
+                  backgroundColor: "rgba(53, 162, 235, 0.5)",
+                },
+              ],
+            }}
+          />
+        </div>
+      </section>
+
+      <section className="relative overflow-hidden bg-white shadow-lg sm:rounded-lg col-span-12 lg:col-span-4">
+        <div className="px-6 py-4 bg-white border-b border-gray-200">
+          <div className="text-xl font-semibold">
+            <div className="flex items-center justify-between">
+              <p className="text-xl font-semibold">NFTs in each category</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Pie
+            data={{
+              labels: categoriesNftsData?.labels,
+              datasets: [
+                {
+                  label: "NFTs",
+                  data: categoriesNftsData.data,
+                  backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                  ],
+                  borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                  ],
+                  borderWidth: 1,
+                },
+              ],
+            }}
+          />
+        </div>
+      </section>
+
+      <section className="flex flex-col col-span-12 gap-4 lg:col-span-4">
+        {[
+          {
+            title: "total KYC Applications",
+            value: kycData.count?.total,
+            color: " bg-gray-500/20 text-gray-700",
+          },
+          {
+            title: "on review KYC Applications",
+            value: kycData.count?.on_review,
+            color: " bg-blue-500/20 text-blue-700",
+          },
+          {
+            title: "approved KYC Applications",
+            value: kycData.count?.approved,
+            color: " bg-green-500/20 text-green-700",
+          },
+          {
+            title: "pending KYC Applications",
+            value: kycData.count?.pending,
+            color: "bg-yellow-500/20 text-yellow-700",
+          },
+          {
+            title: "rejected KYC Applications",
+            value: kycData.count?.rejected,
+            color: "bg-red-500/20 text-red-700",
+          },
+        ].map((item, index) => (
+          <div
+            key={index}
+            className="relative overflow-hidden bg-white shadow-lg sm:rounded-lg"
+          >
+            <div className="px-6 py-4">
+              <div className="flex flex-wrap items-center gap-4">
+                <div
+                  className={`p-4 rounded-lg flex items-center ${item.color}`}
+                >
+                  <i className="bx bx-id-card"></i>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm text-gray-500 uppercase">
+                    {item.title}
+                  </p>
+                  <p className="text-xl font-semibold text-gray-900">
+                    {item.value}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+
+      <section className="relative overflow-hidden bg-white shadow-lg sm:rounded-lg col-span-12 lg:col-span-8">
+        <div className="px-6 py-4 bg-white border-b border-gray-200">
+          <div className="text-xl font-semibold">
+            <div className="flex items-center justify-between">
+              <p className="text-xl font-semibold">KYC Applications</p>
+              <p className="text-sm text-gray-500">Last 30 days</p>
+            </div>
+          </div>
+        </div>
+        <div className="px-6 py-4">
+          {/* chart */}
+          <Line
+            options={{
+              responsive: true,
+              plugins: {
+                legend: {
+                  position: "top" as const,
+                },
+                title: {
+                  display: true,
+                  text: "KYC Applications",
+                },
+              },
+            }}
+            data={{
+              labels: kycData.statistics?.labels,
+              datasets: [
+                {
+                  label: "rejection",
+                  data: kycData.statistics?.rejection,
+                  borderColor: "rgb(255, 99, 132)",
+                  backgroundColor: "rgba(255, 99, 132, 0.5)",
+                },
+                {
+                  label: "approval",
+                  data: kycData.statistics?.approved,
+                  borderColor: "rgb(4, 108, 78)",
+                  backgroundColor: "rgba(4, 108, 78, 0.5)",
+                },
+                {
+                  label: "pending",
+                  data: kycData.statistics?.pending,
+                  borderColor: "rgb(142, 75, 16)",
+                  backgroundColor: "rgba(142, 75, 16, 0.5)",
+                },
+                {
+                  label: "on review",
+                  data: kycData.statistics?.on_review,
+                  borderColor: "rgb(53, 162, 235)",
+                  backgroundColor: "rgba(53, 162, 235, 0.5)",
+                },
+              ],
+            }}
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
